22f0297526bf6d4ced5aa9bd218c9d4d1101e5af
Stefan <1708541+stefankn@users.noreply.github.com>
2026-04-15 12:17:11 +0200

    Apply migrations on startup

--- LinnetServer/Program.cs
+++ LinnetServer/Program.cs
@@ -42,6 +42,12 @@ try
 
     var app = builder.Build();
 
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        await db.Database.MigrateAsync();
+    }
+
     if (!app.Environment.IsDevelopment())
     {
         app.UseExceptionHandler("/Error", createScopeForErrors: true);
